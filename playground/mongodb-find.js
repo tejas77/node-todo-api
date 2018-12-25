d61f1a8206b43b190cc8db29e467a226367caf0a
Tejas Rathod <happytejasrathod@gmail.com>
2018-12-25 20:31:59 +0530

    Init Todo

---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5c223fd795ce2e1fc848743d')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Tejas'
+  }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+  client.close();
+});
