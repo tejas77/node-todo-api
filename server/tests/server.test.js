06ffd93b17884366f01aa149a5e3b8e4cff1a5d2
Tejas Rathod <happytejasrathod@gmail.com>
2019-03-28 16:58:12 +0530

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -97,3 +97,40 @@ describe('GET /todos/:id', () => {
          .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        request(app)
+         .delete(`/todos/${hexId}`)
+         .send()
+         .expect(200)
+         .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+         })
+         .end((err,res) => {
+            if(err) {
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toBeNull();
+                done();
+            }).catch((e) => done(e));
+         });   
+    });
+    
+    it('should return a 404 if todo not found', (done) => {
+        request(app)
+        .delete(`/todos/${todos[0]._id.toHexString()+1}`)
+        .expect(404)
+        .end(done);
+    });
+    
+    it('should return a 404 for non-object ids', (done) => {
+        request(app)
+         .delete('/todos/123')
+         .expect(404)
+         .end(done);
+    });
+});
