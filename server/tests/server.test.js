76de99718fbe2988ba0b988f3727bfbd355eb414
Tejas Rathod <happytejasrathod@gmail.com>
2019-04-12 22:51:14 +0530

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -291,3 +291,23 @@ describe('POST /users/login', () =>{
       });
   });
 });
+
+describe('DELETE /users/me/token', () =>{
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+        done();
+        }).catch((e) => done(e));
+      });
+  });
+
+});
